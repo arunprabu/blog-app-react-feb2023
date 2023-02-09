b0de3d8e18a1b292a1939a6e47df0d8d557a018f
Arun Vijayarengan <arunprabu.v@hotmail.com>
2023-02-09 19:30:38 +0530

    updated

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+// Fn Comp with Anonymous Fn 
+const Footer = function() {
+  return (
+    <footer className="text-center">
+      <hr />
+      <p>Copyright &copy; 2023 | Arun</p>
+    </footer>
+  )
+}
+
+export default Footer;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,58 @@
+// Fn components
+/* 
+  1. Fn Comp with Named Fn [DONE]
+  2. Fn Comp with Anonymous Fn [DONE]
+  3. Fn Comp with Arrow Fn 
+*/
+
+// Fn Comp with Named Fn
+function Header() {
+  return (
+    <header>
+      <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="/">
+            My Blog App
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarCollapse"
+            aria-controls="navbarCollapse"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className="collapse navbar-collapse" id="navbarCollapse">
+            <ul className="navbar-nav me-auto mb-2 mb-md-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="/">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/">
+                  Blog
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/">
+                  About
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/">
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
